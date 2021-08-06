cfb1863d657507266fe38a6f0effbdae0d5ff7a3
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-08-06 15:40:19 +0300

    Added mapStateToProps to connect the Redux state to React props

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -7,4 +7,8 @@ class SongList extends React.Component {
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
